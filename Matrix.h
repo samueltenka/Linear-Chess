7eae58bdd914b4d0012ea8acd9687c6df1238b5f
samueltenka <iguavadon@gmail.com>
2014-07-26 16:49:52 -0400

    Update Matrix.h
    Outline of Matrix arithmetic.

--- Matrix.h
+++ Matrix.h
@@ -4,5 +4,28 @@
 #define MATRIX
 class Matrix
 {
+private:
+	int height, width;
+	Vector row(int r);
+	Vector column(int c);
+
+public:
+	Matrix(int h, int w);
+	Matrix();
+	Matrix operator=(Matrix other); // copy
+	void print();
+
+	Matrix operator+=(Matrix other);
+	Matrix operator-=(Matrix other);
+	Matrix operator*=(Matrix other);
+	Matrix operator*=(Scalar scale);
+	Matrix operator/=(Scalar scale);
+
+	Matrix operator+(Matrix other);
+	Matrix operator-(Matrix other);
+	Matrix operator*(Matrix other); // matrix multiplication
+	Matrix operator*(Scalar scale);
+	Matrix operator/(Scalar scale);
+	Vector operator*(Vector v); // action on vector
 };
 #endif
