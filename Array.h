dd558105f163fdd8a535aeb8a9e0a9a2ceaa8b41
samueltenka <iguavadon@gmail.com>
2014-07-26 16:48:00 -0400

    Update Array.h
    Fleshed-out Array class. Point is to safely wrap dynamic memory allocation.

--- Array.h
+++ Array.h
@@ -4,5 +4,29 @@
 template<typename T>
 class Array
 {
+private:
+	T* data;
+
+public:
+	const int length;
+
+	Array(int l):
+		length(l)
+	{
+		data = new T[l];
+	}
+	Array()
+	{
+	}
+
+	T& operator[](int i) // by reference, so both gets and sets.
+	{
+		return data[i];
+	}
+
+	~Array()
+	{
+		delete[] data;
+	}
 };
 #endif
