f080d279649cb1ea4ddea6d0a080c5c24b1afec1
samueltenka <iguavadon@gmail.com>
2014-07-26 16:36:44 -0400

    Create Main.cpp
    OK, no problems compiling.

---
+++ Main.cpp
@@ -0,0 +1,17 @@
+#include "Scalar.h"
+#include "Array.h"
+#include "Vector.h"
+#include "Matrix.h"
+
+#include <iostream>
+using namespace std;
+
+void main()
+{
+
+
+	// END:
+	cout << "tada!" << endl;
+	char l;
+	cin >> l;
+}
