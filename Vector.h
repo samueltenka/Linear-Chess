099fd6e56051913d4286efbefca2daca616f2996
samueltenka <iguavadon@gmail.com>
2014-07-26 16:49:33 -0400

    Update Vector.h
    Outline of Vector arithmetic.

--- Vector.h
+++ Vector.h
@@ -5,5 +5,27 @@
 #define VECTOR
 class Vector: public Array<Scalar>
 {
+private:
+	int dimension;
+
+public:
+	Vector(int d);
+	Vector();
+	Vector operator=(Vector other); // copy
+	void print();
+
+	Scalar mag();
+	void normalize();
+
+	Vector operator+=(Vector other);
+	Vector operator-=(Vector other);
+	Vector operator*=(Scalar scale);
+	Vector operator/=(Scalar scale);
+
+	Vector operator+(Vector other);
+	Vector operator-(Vector other);	
+	Vector operator*(Scalar scale);
+	Vector operator/(Scalar scale);
+	Scalar operator*(Vector other); // dot product
 };
 #endif
