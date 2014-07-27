481779f243cccd2f7252953d4f78211fa86fdcdb
samueltenka <iguavadon@gmail.com>
2014-07-27 17:33:54 -0400

    Update VectorBasic.cpp
    Implemented data-management changes described in Vector.h.
    
    Also added "print_horizontal", for use by Matrix print-functions.

--- VectorBasic.cpp
+++ VectorBasic.cpp
@@ -3,45 +3,52 @@
 using namespace std;
 
 
+//
+// Data-management (i.e. Dimension-getter, Constructors w/ Initializer, and Copier)
+inline int Vector::dimension() const
+{
+	return length;
+}
+Vector::Vector()
+{
+}
 Vector::Vector(int d):
-	dimension(d),
 	Array(d)
 {
 }
 Vector::Vector(const Vector& other):
-	dimension(other.dimension),
-	Array(other.dimension)
+	Array(other.dimension())
 {
-	for(int r = 0; r < dimension; r++)
-	{
-		(*this)[r] = other[r];
-	}
+	(*this) = other;
 }
 Vector& Vector::operator=(const Vector& other) // copy
 {
-	for(int r = 0; r < dimension; r++)
+	for(int r = 0; r < dimension(); r++)
 	{
 		(*this)[r] = other[r];
 	}
 	return (*this);
 }
+
+//
+// Printing:
 void Vector::print()
 {
-	for(int r = 0; r < dimension; r++)
+	for(int r = 0; r < dimension(); r++)
 	{
-		cout << (*this)[r] << endl;
+		printf("%+f\n", (*this)[r]);
 	}
 }
 void Vector::print_fancy()
 {
-	for(int r = 0; r < dimension; r++)
+	for(int r = 0; r < dimension(); r++)
 	{
 		Scalar value = (*this)[r];
-		if(r == 0) // top
+		if(r == 0) // top:
 		{
 			printf(" / %+f \\ \n", value);
 		}
-		else if(r < dimension-1) // main body:
+		else if(r < dimension()-1) // main body:
 		{
 			printf("|  %+f  |\n", value);
 		}
@@ -49,5 +56,12 @@ void Vector::print_fancy()
 		{
 			printf(" \\ %+f / \n", value);
 		}
+	}
+}
+void Vector::print_horizontal()
+{
+	for(int r = 0; r < dimension(); r++)
+	{
+		printf("%+f ", (*this)[r]);
 	}
 }
