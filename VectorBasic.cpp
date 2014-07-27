2dba31880820249380c90d89690dc8cb42a30613
samueltenka <iguavadon@gmail.com>
2014-07-26 21:12:49 -0400

    Update VectorBasic.cpp
    Made operator= return by-reference, fixing bug described in Vector.h update.

--- VectorBasic.cpp
+++ VectorBasic.cpp
@@ -2,21 +2,19 @@
 #include <iostream>
 using namespace std;
 
-Vector::Vector()
-{
-}
+
 Vector::Vector(int d):
 	dimension(d),
 	Array(d)
 {
 }
-Vector Vector::operator=(Vector other) // copy
+Vector& Vector::operator=(Vector other) // copy
 {
 	for(int r = 0; r < dimension; r++)
 	{
 		(*this)[r] = other[r];
 	}
-	return (*this);
+	return *this;
 }
 void Vector::print()
 {
@@ -32,15 +30,15 @@ void Vector::print_fancy()
 		Scalar value = (*this)[r];
 		if(r == 0) // top
 		{
-			printf(" / %+*f \\ \n", 5, value);
+			printf(" / %+f \\ \n", value);
 		}
 		else if(r < dimension-1) // main body:
 		{
-			printf("|  %+*f  |\n", 5, value);
+			printf("|  %+f  |\n", value);
 		}
 		else // bottom:
 		{
-			printf(" \\ %+*f / \n", 5, value);
+			printf(" \\ %+f / \n", value);
 		}
 	}
 }
