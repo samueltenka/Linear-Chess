f07f9ee3c7dfe8f681e5488052252e7591b3c34c
samueltenka <iguavadon@gmail.com>
2014-07-27 17:36:29 -0400

    Update Matrix.h
    Fleshed out Matrix class in style of Vector class:
    
    Now it's an Array of Vectors,
    with (implemented) constructors, copier, and printing,
    and (unimplemented) by-reference, const-where-appropriate arithmetic.

--- Matrix.h
+++ Matrix.h
@@ -2,30 +2,35 @@
 
 #ifndef MATRIX
 #define MATRIX
-class Matrix
+class Matrix: public Array<Vector>	// array of rows
+									// so access as M[r][c]
 {
 private:
 	int height, width;
-	Vector row(int r);
-	Vector column(int c);
+
+	Vector row(int r); // used for arithmetic
+	Vector column(int c); //  ` ` `
 
 public:
-	Matrix(int h, int w);
-	Matrix();
-	Matrix& operator=(Matrix other); // copy
-	void print();
 
-	Matrix& operator+=(Matrix other);
-	Matrix& operator-=(Matrix other);
-	Matrix& operator*=(Matrix other);
+	Matrix(int h, int w);
+	Matrix(const Matrix& other);
+	Matrix& operator=(const Matrix& other); // copy
+
+	Matrix& operator+=(const Matrix& other);
+	Matrix& operator-=(const Matrix& other);
+	Matrix& operator*=(const Matrix& other);
 	Matrix& operator*=(Scalar scale);
 	Matrix& operator/=(Scalar scale);
 
-	Matrix operator+(Matrix other);
-	Matrix operator-(Matrix other);
-	Matrix operator*(Matrix other); // matrix multiplication
+	Matrix operator+(const Matrix& other);
+	Matrix operator-(const Matrix& other);
+	Matrix operator*(const Matrix& other); // matrix multiplication
 	Matrix operator*(Scalar scale);
 	Matrix operator/(Scalar scale);
-	Vector operator*(Vector v); // action on vector
+	Vector operator*(const Vector& v); // action on vector
+
+	void print();
+	void print_fancy();
 };
 #endif
