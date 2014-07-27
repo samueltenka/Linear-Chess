3a7cfdf2041322be0948bf835366315c82dfad8e
samueltenka <iguavadon@gmail.com>
2014-07-26 23:58:25 -0400

    Update Vector.h
    Pass Vectors by reference, and constant.

--- Vector.h
+++ Vector.h
@@ -1,6 +1,7 @@
 #include "Scalar.h"
 #include "Array.h"
 
+
 #ifndef VECTOR
 #define VECTOR
 class Vector: public Array<Scalar>
@@ -11,22 +12,23 @@ private:
 public:
 	Vector(int d);
 	Vector();
-	Vector& operator=(Vector other); // copy
+	Vector& operator=(Vector& other); // copy
 	void print();
 	void print_fancy();
 
 	Scalar mag();
 	void normalize();
 
-	Vector& operator+=(Vector other);
-	Vector& operator-=(Vector other);
+	Vector& operator+=(const Vector& other);
+	Vector& operator-=(const Vector& other);
 	Vector& operator*=(Scalar scale);
 	Vector& operator/=(Scalar scale);
 
-	Vector operator+(Vector other);
-	Vector operator-(Vector other);	
+	Vector operator+(const Vector& other);
+	Vector operator-(const Vector& other);	
 	Vector operator*(Scalar scale);
 	Vector operator/(Scalar scale);
-	Scalar operator*(Vector other); // dot product
+
+	Scalar operator*(const Vector& other); // dot product
 };
 #endif
