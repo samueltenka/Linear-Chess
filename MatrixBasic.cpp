16c854f34c280e5096e2e358c6397e2029780527
samueltenka <iguavadon@gmail.com>
2014-07-27 17:37:28 -0400

    Create MatrixBasic.cpp
    Implements constructors, copier, and printers specified in Matrix.h.

---
+++ MatrixBasic.cpp
@@ -0,0 +1,79 @@
+#include "Matrix.h"
+#include <iostream>
+using namespace std;
+
+
+//
+// Constructors:
+Matrix::Matrix(int h, int w):
+	height(h),
+	width(w),
+	Array(h) // array of rows
+{
+	// initialize rows:
+	for(int r = 0; r < height; r++)
+	{
+		(*this)[r].initialize(h);
+	}
+}
+Matrix::Matrix(const Matrix& other):
+	height(other.height),
+	width(other.width),
+	Array(other.height) // array of rows
+{
+	// initialize rows:
+	for(int r = 0; r < height; r++)
+	{
+		(*this)[r].initialize(height);
+	}
+
+	// copy:
+	(*this) = other;
+}
+
+//
+// Copier:
+Matrix& Matrix::operator=(const Matrix& other)
+{
+	for(int r = 0; r < height; r++)
+	{
+		for(int c = 0; c < width; c++)
+		{
+			(*this)[r] = other[r];
+		}
+	}
+	return (*this);
+}
+
+//
+// Print functions:
+void Matrix::print()
+{
+	for(int r = 0; r < height; r++)
+	{
+		(*this)[r].print_horizontal();
+		cout << endl;
+	}
+}
+void Matrix::print_fancy()
+{
+	for(int r = 0; r < height; r++)
+	{
+		Vector row = (*this)[r];
+
+		if(r == 0) // top:
+		{
+			cout << " / "; row.print_horizontal(); cout << "\\ ";
+		}
+		else if(r < height-1) // main body:
+		{
+			cout << "|  "; row.print_horizontal(); cout << " |";
+		}
+		else // bottom:
+		{
+			cout << " \\ "; row.print_horizontal(); cout << "/ ";
+		}
+
+		cout << endl;
+	}
+}
