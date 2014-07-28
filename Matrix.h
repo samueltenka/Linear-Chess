c01d6da002ab4eebbaef8c166597ee32d0bed764
samueltenka <iguavadon@gmail.com>
2014-07-28 02:39:07 -0400

    Update Matrix.h
    Specified "const" for all appropriate methods, and outlined eigenvector functionality:
    * eigenvectors(), which repeatedly calls
    *   biggest_eigenvector() and
    *   remove_from_span(...), and whose output can be seen by means of
    *   print_rowvectors()

--- Matrix.h
+++ Matrix.h
@@ -8,11 +8,10 @@ class Matrix: public Array<Vector>	// array of rows
 private:
 	int height, width;
 
-	Vector row(int r) const; // used for arithmetic
-	Vector column(int c) const; //  ` ` `
+	Vector row(int r) const;	// used for arithmetic
+	Vector column(int c) const;	//  ` ` `
 
 public:
-
 	Matrix(int h, int w);
 	Matrix(const Matrix& other);
 	Matrix& operator=(const Matrix& other); // copy
@@ -23,14 +22,22 @@ public:
 	Matrix& operator*=(Scalar scale);
 	Matrix& operator/=(Scalar scale);
 
-	Matrix operator+(const Matrix& other);
-	Matrix operator-(const Matrix& other);
-	Matrix operator*(const Matrix& other); // matrix multiplication
-	Matrix operator*(Scalar scale);
-	Matrix operator/(Scalar scale);
-	Vector operator*(const Vector& v); // action on vector
+	Matrix operator+(const Matrix& other) const;
+	Matrix operator-(const Matrix& other) const;
+	Matrix operator*(const Matrix& other) const; // matrix multiplication
+	Matrix operator*(Scalar scale) const;
+	Matrix operator/(Scalar scale) const;
+	Vector operator*(const Vector& v) const; // action on vector
 
-	void print();
-	void print_fancy();
+private:
+	void remove_from_span(Vector target);
+	Vector biggest_eigenvector() const; // probabilistic: might not exit, and might not be biggest.
+public:
+	Matrix eigenvectors() const;	// rows of rtrn are (orthonormal) eigenvectors,
+									// so that V*rtrn changes standard coordinates into eigenbasis-coordinates.
+
+	void print() const;
+	void print_fancy() const;
+	void print_rowvectors() const;
 };
 #endif
