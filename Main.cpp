1412f362a2f8be926a0fcc93e51936b46b7a0b05
samueltenka <iguavadon@gmail.com>
2014-07-29 00:00:49 -0400

    Update Main.cpp
    PCA works!
    
    Output:
    The principal components of the following dataset
     / 0.0000 \
    |  0.0000  |
     \ 0.0000 /
    
     / 1.0000 \
    |  1.0000  |
     \ 1.0000 /
    
     / 2.0000 \
    |  2.0000  |
     \ 2.0000 /
    
     / 1.1000 \
    |  0.9000  |
     \ 1.0000 /
    
     / 1.2000 \
    |  0.8000  |
     \ 1.0010 /
    
    are:
     /  0.5774  0.5773  0.5774 \
    |   0.7060 -0.7082  0.0020  |
     \  0.2555 -0.7994  0.5437 /
    or, more intuitively scaled:
     /  1.0000  1.0000  1.0000 \
    |   1.0000 -1.0031  0.0028  |
     \  0.4699 -1.4702  1.0000 /
    tada!

--- Main.cpp
+++ Main.cpp
@@ -9,19 +9,53 @@ using namespace std;
 
 void main()
 {
-	// TEST MatrixEigen.cpp on large matrices:
-	Matrix M(100, 100);
-	for(int r = 0; r < 100; r++)
-	{
-		for(int c = 0; c <= r; c++)
-		{
-			M[c][r] = M[r][c] = rand()%5;
-		}
-	}
-	cout << "First two eigenvectors of M" << endl;
-	cout << "are:" << endl;
-	Matrix E = M.eigenvectors(2);
-	E.print_fancy();
+	// TEST PCA.cpp:
+	Matrix M(5, 3);
+	Vector	V0(3),
+			V1(3),
+			V2(3), 
+			V3(3), 
+			V4(3);
+	V0[0] = 0.0;
+	V0[1] = 0.0;
+	V0[2] = 0.0;
+
+	V1[0] = 1.0;
+	V1[1] = 1.0;
+	V1[2] = 1.0;
+
+	V2[0] = 2.0;
+	V2[1] = 2.0;
+	V2[2] = 2.0;
+
+	V3[0] = 1.1;
+	V3[1] = 0.9;
+	V3[2] = 1.0;
+
+	V4[0] = 1.2;
+	V4[1] = 0.8;
+	V4[2] = 1.001;
+
+	M[0] = V0;
+	M[1] = V1;
+	M[2] = V2;
+	M[3] = V3;
+	M[4] = V4;
+	
+	cout << "The principal components of the following dataset " << endl;
+	M.print_rowvectors();
+	
+	Matrix P = M.principal_components(3);
+	cout << "are: " << endl;
+	P.print_fancy();
+	
+	cout << "or, more intuitively scaled: " << endl;
+	P.print_fancy_intuitive();
+
+	Matrix C(M);
+	//C.center();
+	//C.print_fancy();
+	
 
 	// END:
 	cout << "tada!" << endl;
