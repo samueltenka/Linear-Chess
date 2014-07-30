c180388b4bdb5ca7be9b6914a0aace7ab3bda133
samueltenka <iguavadon@gmail.com>
2014-07-30 15:24:13 -0400

    Update ChessPosition.h
    Declared "move" and "from_algebraic". Usage:
    C.move(C.from_algebraic("pe4"))
    pushes pawn up.
    
    Also, wrapped previous content of main into "display": writing a bitmap representation of state() to given filename.

--- ChessPosition.h
+++ ChessPosition.h
@@ -22,9 +22,9 @@ public:
 	ChessPosition();
 	~ChessPosition();
 	void set_up();
-	Piece* piece_at(Square square);
+	Piece*& piece_at(Square square);
 
-public:
+private:
 	LinkedList<Square> ray_from(Square source, int y_inc, int x_inc,								// geometry
 								int max_length, Piece piece, bool takingOK, bool taking_required);	// stopping requirements (handles Kings, Queens, Rooks, Bishops, Pawns)
 																									// (piece is needed to compare colors with destinations)
@@ -32,7 +32,13 @@ public:
 public:
 	LinkedList<Move> possible_moves();
 
+private:
 	ChessVector state(); // some weight to possible moves.
+public:
+	void display(const char* filename);
 	void print();
+
+	void move(Move m);
+	Move from_algebraic(const char* algebraic);
 };
 #endif
