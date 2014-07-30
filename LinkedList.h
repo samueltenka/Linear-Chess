ee1033c5dbb4f9f37b02f7e03baf51eb6f8983a8
samueltenka <iguavadon@gmail.com>
2014-07-29 21:41:34 -0400

    Update LinkedList.h
    Revised whole file, fixing typos such as that in "void prev() {node = node->successor}", and simplifying methods such as destructor (wherein we changed end-condition to include previous "delete end" statement.).
    
    Note that "start(end)" in initializer list doesn't seem to set start to be the same as end (?why? I don't understand initializer lists very well??); so we just initialized "start" in the body.
    
    Also, major change: problem with old "append(list)" --- see comments --- so renamed to "join" and created a slower version --- "append" --- that works by copying whole list.

--- LinkedList.h
+++ LinkedList.h
@@ -38,10 +38,11 @@ private:
 	LLNode<T>* node;
 	LLIterator(LLNode<T>* n): node(n) {}
 public:
-	LLIterator(LinkedList<T>& LL):
-		node(LL.start) {}
-	bool can_continue() {return node->successor != NULL;}
-	bool can_regress() {return node->successor != NULL;}
+	LLIterator(LinkedList<T>& LL): node(LL.start) {}
+	bool can_continue() {
+		return node->successor != NULL;
+	}
+	bool can_regress() {return node->predecessor != NULL;}
 	void next() {node = node->successor;}
 	void prev() {node = node->predecessor;}
 	T& value() {return node->value;}
@@ -50,44 +51,67 @@ public:
 template<typename T>
 class LinkedList
 {
-private:
+public:
 	template<typename T>
 	friend class LLIterator;
-	LLNode<T>* start;	// half-open: [start, end); in particular,
-	LLNode<T>* end;		// end doesn't contain anything itself, and 
-						// end->successor == NULL.
+	LLNode<T>* start;		// half-open: [start, end); in particular,
+	LLNode<T>* end;			// end doesn't contain anything itself, and 
+							// end->successor == NULL.
 
 public:
-	LinkedList()
-	{
-		end = new LLNode<T>;
-		start = end;
-	}
+	LinkedList(): end(new LLNode<T>) {start = end;}	// or could do "start(new LLNode<T>), end(start)",
+													// but this way jives with phrasing in "append".
+	LinkedList(const LinkedList<T>& other): start(other.start), end(other.end) {cout << "bei";}
+
 	void append(T element)	// don't worry about loops forming: all the node stuff is
 	{						// managed behind the scenes. We merely input a value.
 											// What happens when "[1, 2, 4].append(8)":
 											// s1<-->2<-->4<-->e
 		end->value = element;				// s1<-->2<-->4<-->e8
-		end->successor = new LLNode<T>();   // s1<-->2<-->4<-->e8 -->
+		end->successor = new LLNode<T>;		// s1<-->2<-->4<-->e8 -->
 		end->successor->predecessor = end;	// s1<-->2<-->4<-->e8<-->
 		end = end->successor;				// s1<-->2<-->4<--> 8<-->e
 	}
 	~LinkedList()
 	{
-		for(LLNode<T>* i = start; i != end; )	// could use iterator, but this is easier,
-		{										//
-			LLNode<T>* next = i->successor;		// since an iterator won't let you
-			delete i;							// delete before
-			i = next;							// incrementing.
+		for(LLNode<T>* i = start; i != NULL; )	// could use iterator, but this is easier,
+		{										// <-- i5<--> 6<-->...	//
+			LLNode<T>* next = i->successor;		// <-- i5<-->n6<-->...	// since an iterator won't let you
+			delete i;							//       <-- n6<-->...	// delete before
+			i = next;							//       <-- i6<-->...	// incrementing.
 		}
-		delete end;
 	}
 
-	void append(LinkedList& LL) // to save time, appending a whole list does NOT copy it.
-	{							// so if we later delete LL, end will still be LL.end!
-		end->successor = LL.start;
-		LL.start->predecessor = end;
-		end = LL.end;
+	inline bool is_empty() {return start==end;}
+
+	void join(LinkedList& LL)	// Sets both (*this) and LL to concatenated list.
+	{							// to save time, does NOT copy LL.
+								// However, problem: if L.join(Y), then after L destroyed, destruction of Y raises error.
+		if(is_empty()) (*this) = LL;		// need to check for empty cases.
+		else if(LL.is_empty()) LL = *this;	// ` ` ` ` ` `
+		else											// What happens when "[1, 2, 4].append([3, 5])"?
+		{												//  s1<-->2<-->4<-->e
+														//               S3<-->5<-->E
+			
+			end->predecessor->successor = LL.start;		//  s1<-->2<-->4<-- e
+			LL.start->predecessor = end->predecessor;	//             ^
+														//              \
+														//               v
+														//               S3<-->5<-->E
+
+			delete end;	end = LL.end;					//  sS1<-->2<-->4
+						LL.start = start;				//              ^
+														//               \
+														//                v
+														//                 3<-->5<-->eE
+		}
+	}
+	void append(LinkedList& LL)	// copies LL
+	{
+		for(LLIterator<int> i(LL); i.can_continue(); i.next())
+		{
+			append(i.value());
+		}
 	}
 };
 #endif
