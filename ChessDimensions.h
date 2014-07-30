edda699f688d208b8e7a83064d31cb0a83ffc07b
samueltenka <iguavadon@gmail.com>
2014-07-30 03:48:49 -0400

    Update ChessDimensions.h
    Outlined new methods for use in move-filtering:
    empty_on, on_board, can_arrive_at.

--- ChessDimensions.h
+++ ChessDimensions.h
@@ -1,9 +1,14 @@
 #ifndef CHESS_DIMENSIONS
 #define CHESS_DIMENSIONS
 
-#define NUM_SQUARES 64
-#define NUM_PIECES 12
+#define SIDELENGTH 8
+#define NUM_SQUARES (SIDELENGTH*SIDELENGTH)
+#define NUM_COLORS 2
+#define NUM_SPECIES 6
+#define NUM_PIECES (NUM_COLORS*NUM_SPECIES)
 #define NUM_DIMENSIONS (NUM_SQUARES*NUM_PIECES)
+
+class ChessPosition;
 
 struct Square
 {
@@ -12,10 +17,13 @@ struct Square
 	Square() {}
 	Square(int index)
 	{
-		file = index%8; index /= 8;
-		rank = index%8; 
+		file = index%SIDELENGTH; index /= SIDELENGTH;
+		rank = index%SIDELENGTH;
 	}
 	Square(int r, int f): rank(r), file(f) {}
+
+	inline bool empty_on(ChessPosition& CP);
+	inline bool on_board();
 };
 enum Color {black, white};
 enum Species {pawn, knight, bishop, rook, queen, king};
@@ -23,26 +31,29 @@ struct Piece
 {
 	Color color;
 	Species species;
-	inline int index() {return 6*color + species;}
+	inline int index() {return NUM_SPECIES*color + species;}
 	Piece() {}
 	Piece(int index)
 	{
-		species = static_cast<Species>(index%6); index /= 6;
-		color = static_cast<Color>(index%2); index /= 2;
+		species = static_cast<Species>(index%NUM_SPECIES); index /= NUM_SPECIES;
+		color = static_cast<Color>(index%NUM_COLORS);
 	}
 	Piece(Color c, Species s): color(c), species(s) {}
+
+	inline bool can_arrive_at(Square destination, ChessPosition& CP, bool takingOK, bool taking_required); // needs piece's color info
 };
 struct Dimension
 {
 	Square square;
 	Piece piece;
 	inline int index() {return NUM_PIECES*square.index() + piece.index();}
+	Dimension() {}
 	Dimension(int index)
 	{
-		piece.species = static_cast<Species>(index%6); index /= 6;
-		piece.color = static_cast<Color>(index%2); index /= 2;
-		square.file = index%8; index /= 8;
-		square.rank = index%8;
+		piece.species = static_cast<Species>(index%NUM_SPECIES); index /= NUM_SPECIES;
+		piece.color = static_cast<Color>(index%NUM_COLORS); index /= NUM_COLORS;
+		square.file = index%SIDELENGTH; index /= SIDELENGTH;
+		square.rank = index%SIDELENGTH;
 	}
 	Dimension(int r, int f, Color c, Species s): square(r, f), piece(c, s) {}
 	Dimension(Square S, Piece P): square(S), piece(P) {}
