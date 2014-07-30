0633d8393107e9b99169880c78c7367ee74fdff0
samueltenka <iguavadon@gmail.com>
2014-07-30 03:51:59 -0400

    Update ChessPosition.h
    Where once was ChessVector class is now a more classical ChessPosition class: 1 or 0 pieces at each square, leading to efficient move-generation. Indeed, possible_moves is a pretty nifty method that returns a list of of all legal source-destination tuples.
    
    (now, CV class is in ChessVector.h)

--- ChessPosition.h
+++ ChessPosition.h
@@ -1,23 +1,38 @@
-#include "Vector.h"
 #include "ChessDimensions.h"
-#include "GraphicsBitmap.h"
+#include "ChessVector.h"
+#include "LinkedList.h"
 
 
 #ifndef CHESS_POSITION
 #define CHESS_POSITION
-class ChessPosition
+
+struct Move
+{
+	Square source, destination;
+	Move(Square s, Square d): source(s), destination(d) {}
+	Move() {}
+};
+
+class ChessPosition // classical, i.e. exactly 1 or 0 pieces at each square
 {
 private:
-	Vector as_vector;
+	Piece* board[8][8]; // pointer to allow for empty. 
+
 public:
 	ChessPosition();
-	ChessPosition(const ChessPosition& other);
+	~ChessPosition();
 	void set_up();
+	Piece* piece_at(Square square);
 
-	Scalar& component(Dimension d);
+public:
+	LinkedList<Square> ray_from(Square source, int y_inc, int x_inc,								// geometry
+								int max_length, Piece piece, bool takingOK, bool taking_required);	// stopping requirements (handles Kings, Queens, Rooks, Bishops, Pawns)
+																									// (piece is needed to compare colors with destinations)
+	LinkedList<Square> possible_destinations(Square source, Piece piece);
+public:
+	LinkedList<Move> possible_moves();
 
-	void move(Square source, Square destination);
-
-	void paint(Bitmap& C);
+	ChessVector state(); // some weight to possible moves.
+	void print();
 };
 #endif
